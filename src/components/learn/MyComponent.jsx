02e97c60793cb98053bcd315539246c67143791c
TRAN VIET SON <tranvietson1983@gmail.com>
2024-10-22 21:57:37 +0700

    video 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,10 +1,20 @@
 import './style.css';
 //component = html + css + js
 const MyComponent = () => {
+
+    //const hoidanit = "eric channel";
+    //const hoidanit = true;
+    //const hoidanit = undefined;
+    //const hoidanit = null;
+    // const hoidanit = [1, 2, 3];
+    const hoidanit = {
+        name: "hoidanit",
+        age: 25
+    }
     return (
         <>
-
-            <div>Hoi dan it with eric!</div>
+            <div>Hoi dan it with {JSON.stringify(hoidanit)}!</div>
+            <div>{console.log("eric!!!")}</div>
             <div className='child'
                 style={
                     { borderRadius: "10px" }
