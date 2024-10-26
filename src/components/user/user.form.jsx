048622ef6c6ff4859197f4ee0a604a8b54285f55
TRAN VIET SON <tranvietson1983@gmail.com>
2024-10-26 16:05:05 +0700

    video 67

---
+++ src/components/user/user.form.jsx
@@ -0,0 +1,34 @@
+import { Input } from "antd";
+import { Button } from "antd";
+
+const UserForm = () => {
+    return (
+        <div className="user-form" style={{ margin: "20px 0" }}>
+            <div style={{ display: "flex", gap: "15px", flexDirection: "column" }}>
+                <div>
+                    <span>Full Name</span>
+                    <Input />
+                </div>
+                <div>
+                    <span>Email</span>
+                    <Input />
+                </div>
+                <div>
+                    <span>Password</span>
+                    <Input />
+                </div>
+                <div>
+                    <span>Phone number</span>
+                    <Input />
+                </div>
+                <div>
+                    <Button type="primary">Create User</Button>
+                </div>
+            </div>
+
+
+        </div>
+    )
+}
+
+export default UserForm;
