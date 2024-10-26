acc7f4368a09d3168c623d9f3cc73081d6e69745
TRAN VIET SON <tranvietson1983@gmail.com>
2024-10-26 09:44:26 +0700

    video 49

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,11 +1,12 @@
+import { Link } from 'react-router-dom';
 import './header.css';
 
 const Header = () => {
     return (
         <ul>
-            <li><a class="active" href="/">Home</a></li>
-            <li><a href="/users">Users</a></li>
-            <li><a href="/product">Product</a></li>
+            <li><Link class="active" to="/">Home</Link></li>
+            <li><Link to="/users">Users</Link></li>
+            <li><Link to="/product">Product</Link></li>
         </ul>
     )
 }
