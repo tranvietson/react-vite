2b9c9819a0ce168907fafda0a81f2548017ab9da
TRAN VIET SON <tranvietson1983@gmail.com>
2024-11-04 21:21:38 +0700

    video 82

--- src/components/user/view.user.detail.jsx
+++ src/components/user/view.user.detail.jsx
@@ -1,4 +1,4 @@
-import { Drawer } from "antd";
+import { Button, Drawer } from "antd";
 
 const ViewUserDetail = (props) => {
 
@@ -7,6 +7,7 @@ const ViewUserDetail = (props) => {
     return (
         <>
             <Drawer
+                width={"30vw"}
                 title="Basic Drawer"
                 onClose={() => {
                     setIsOpenDrawer(false);
@@ -25,6 +26,26 @@ const ViewUserDetail = (props) => {
                         <br />
                         <p>{dataUserDetail.phone}</p>
                         <br />
+                        <p>Avatar:</p>
+                        <div>
+                            <img height={100} width={150}
+                                src={`${import.meta.env.VITE_BACKEND_URL}/images/avatar/${dataUserDetail.avatar}`} />
+                        </div>
+                        <div>
+                            <label htmlFor="btnUpload"
+                                style={{
+                                    display: "block",
+                                    width: "fit-content",
+                                    marginTop: "15px",
+                                    padding: "5px 10px",
+                                    background: "orange",
+                                    borderRadius: "5px",
+                                    cursor: "pointer"
+                                }}
+                            >Upload avatar</label>
+                            <input type='file' hidden id='btnUpload' />
+                        </div>
+                        <Button type='primary'>Upload Avatar</Button>
                     </>
                     :
                     <div>have not data</div>
