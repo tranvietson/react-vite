15e1326af1df1085bc4a4be699c427d94557c8a1
TRAN VIET SON <tranvietson1983@gmail.com>
2024-11-10 22:48:44 +0700

    video 89

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,7 +1,81 @@
+import { Input, notification, Modal, Button, Form } from "antd";
 
 const RegisterPage = () => {
+    const [form] = Form.useForm();
+    console.log('>>>>>>>> gia tri Form:', Form.useForm());
+    const onFinish = (values) => {
+        console.log('>>>>>>> Check onFinish function:', values);
+    }
+
     return (
-        <div>Register Page</div>
+        <Form
+            form={form}
+            layout="vertical"
+            name="basic"
+            onFinish={onFinish}
+        // onFinishFailed={onFinishFailed}
+        // autoComplete="off"
+        >
+            <div style={{ display: "flex", gap: "15px", flexDirection: "column", marginLeft: "50px" }}>
+                <Form.Item
+                    label="Full Name"
+                    name="fullName"
+                // rules={[
+                //     {
+                //         required: true,
+                //         message: 'Please input your username!',
+                //     },
+                // ]}
+                >
+                    <Input />
+                </Form.Item>
+
+                <Form.Item
+                    label="Email"
+                    name="email"
+                // rules={[
+                //     {
+                //         required: true,
+                //         message: 'Please input your username!',
+                //     },
+                // ]}
+                >
+                    <Input />
+                </Form.Item>
+
+                <Form.Item
+                    label="Password"
+                    name="password"
+                // rules={[
+                //     {
+                //         required: true,
+                //         message: 'Please input your username!',
+                //     },
+                // ]}
+                >
+                    <Input.Password />
+                </Form.Item>
+
+                <Form.Item
+                    label="Phone number"
+                    name="phone"
+                // rules={[
+                //     {
+                //         required: true,
+                //         message: 'Please input your username!',
+                //     },
+                // ]}
+                >
+                    <Input />
+                </Form.Item>
+
+                {/* <button type="submit">Register</button> */}
+                <div>
+                    <Button onClick={() => { form.submit() }} type="primary">Register</Button>
+                </div>
+            </div>
+
+        </Form>
     )
 }
 
