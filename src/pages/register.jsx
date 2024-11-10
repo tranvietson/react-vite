4ca4a95f313925fd9097636c47fb62761b93cd78
TRAN VIET SON <tranvietson1983@gmail.com>
2024-11-10 23:24:13 +0700

    video 90

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,10 +1,32 @@
 import { Input, notification, Modal, Button, Form } from "antd";
+import { registerUserAPI } from "../services/api.service";
+import { useNavigate } from "react-router-dom";
+
 
 const RegisterPage = () => {
     const [form] = Form.useForm();
+    const navigate = useNavigate();
     console.log('>>>>>>>> gia tri Form:', Form.useForm());
-    const onFinish = (values) => {
-        console.log('>>>>>>> Check onFinish function:', values);
+    const onFinish = async (values) => {
+        //  console.log('>>>>>>> Check onFinish function:', values);
+        const res = await registerUserAPI(
+            values.fullName,
+            values.email,
+            values.password,
+            values.phone
+        );
+        if (res.data) {
+            notification.success({
+                message: "Register user",
+                description: "Đăng ký user thành công"
+            });
+            navigate("/login")
+        } else {
+            notification.error({
+                message: "Register user error",
+                description: JSON.stringify(res.message)
+            })
+        }
     }
 
     return (
@@ -20,12 +42,12 @@ const RegisterPage = () => {
                 <Form.Item
                     label="Full Name"
                     name="fullName"
-                // rules={[
-                //     {
-                //         required: true,
-                //         message: 'Please input your username!',
-                //     },
-                // ]}
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your username!',
+                        },
+                    ]}
                 >
                     <Input />
                 </Form.Item>
@@ -33,12 +55,12 @@ const RegisterPage = () => {
                 <Form.Item
                     label="Email"
                     name="email"
-                // rules={[
-                //     {
-                //         required: true,
-                //         message: 'Please input your username!',
-                //     },
-                // ]}
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your username!',
+                        },
+                    ]}
                 >
                     <Input />
                 </Form.Item>
@@ -46,12 +68,12 @@ const RegisterPage = () => {
                 <Form.Item
                     label="Password"
                     name="password"
-                // rules={[
-                //     {
-                //         required: true,
-                //         message: 'Please input your username!',
-                //     },
-                // ]}
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your username!',
+                        },
+                    ]}
                 >
                     <Input.Password />
                 </Form.Item>
@@ -59,12 +81,14 @@ const RegisterPage = () => {
                 <Form.Item
                     label="Phone number"
                     name="phone"
-                // rules={[
-                //     {
-                //         required: true,
-                //         message: 'Please input your username!',
-                //     },
-                // ]}
+                    rules={[
+                        {
+                            // required: true,
+
+                            pattern: new RegExp(/\d+/g),
+                            message: "Wrong format!"
+                        }
+                    ]}
                 >
                     <Input />
                 </Form.Item>
