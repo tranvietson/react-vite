2cc3926e8c7a979d422597f86a16a627ab4b5313
TRAN VIET SON <tranvietson1983@gmail.com>
2024-11-17 18:14:25 +0700

    video 99

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -2,49 +2,57 @@ import React, { useContext, useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 //import './header.css';
 
-import { BookOutlined, HomeOutlined, UsergroupAddOutlined, SettingOutlined } from '@ant-design/icons';
+import { BookOutlined, HomeOutlined, UsergroupAddOutlined, SettingOutlined, AliwangwangOutlined, LoginOutlined } from '@ant-design/icons';
 import { Menu } from 'antd';
 import { AuthContext } from '../context/auth.context';
-const items = [
-    {
-        label: <Link to={"/"}>Home</Link >,
-        key: 'home',
-        icon: <HomeOutlined />,
-    },
-    {
-        label: <Link to={"/users"} >Users</ Link >,
-        key: 'users',
-        icon: <UsergroupAddOutlined />,
 
-    },
-    {
-        label: <Link to={"/books"}>Books</Link >,
-        key: 'books',
-        icon: <BookOutlined />,
-
-    },
-    {
-        label: 'Cài đặt',
-        key: 'setting',
-        icon: <SettingOutlined />,
-        children: [
-            {
-                label: <Link to={"/login"}>Đăng nhập</Link>,
-                key: 'Login',
-            },
-            {
-                label: "Đăng xuất",
-                key: 'logout',
-            }
-        ]
-    }
-
-];
 
 const Header = () => {
     const [current, setCurrent] = useState('mail');
 
     const { user } = useContext(AuthContext);
+
+    const items = [
+        {
+            label: <Link to={"/"}>Home</Link >,
+            key: 'home',
+            icon: <HomeOutlined />,
+        },
+        {
+            label: <Link to={"/users"} >Users</ Link >,
+            key: 'users',
+            icon: <UsergroupAddOutlined />,
+
+        },
+        {
+            label: <Link to={"/books"}>Books</Link >,
+            key: 'books',
+            icon: <BookOutlined />,
+
+        },
+        ...(!user.id ? [
+            {
+                label: <Link to={"/login"}>Đăng nhập</Link >,
+                key: 'login',
+                icon: <LoginOutlined />,
+
+            }] : []),
+
+        ...(user.id ? [
+            {
+                label: `Welcome ${user.fullName}`,
+                key: 'setting',
+                icon: <AliwangwangOutlined />,
+                children: [
+                    {
+                        label: "Đăng xuất",
+                        key: 'logout',
+                    }
+                ]
+            }] : []),
+    ];
+
+
     console.log(">>>>> check data Authcontext:", user);
 
     const onClick = (e) => {
